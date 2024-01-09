d8ae7a4b0652bc7dbdaae67f37dc8d674fddcd39
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-01-09 21:21:11 +0545

    using google navbar

--- google_navbar/lib/homepage.dart
+++ google_navbar/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,15 +11,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Google Navbar'),
-        centerTitle: true,
-      ),
-
-      bottomNavigationBar: Container(
-       child:const GNav(tabs: []),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Google Navbar',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 20,
+            ),
+            child: GNav(
+              gap: 8,
+              backgroundColor: Colors.black,
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade800,
+              padding: const EdgeInsets.all(15),
+              onTabChange: (index) {
+                print(index);
+              },
+              tabs: const [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.favorite_border,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- google_navbar/lib/main.dart
+++ google_navbar/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Google Navbar',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
