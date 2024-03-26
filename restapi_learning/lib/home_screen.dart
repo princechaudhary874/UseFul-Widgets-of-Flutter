5894064d2bb1236fcd5e891dd6eda5279f661661
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-03-26 16:29:54 +0545

    learning upto json to dart

---
+++ restapi_learning/lib/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
