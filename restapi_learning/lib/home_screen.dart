252b21b83891a169082efb70ec61137587cdde45
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-03-27 13:23:07 +0545

    learnig basics concepts of get api

--- restapi_learning/lib/home_screen.dart
+++ restapi_learning/lib/home_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:restapi_learning/models/posts_models.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -8,8 +13,57 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postlist = [];
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postlist.add(PostModel.fromJson(i));
+      }
+      return postlist;
+    } else {
+      return postlist;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Api Tutorial"),
+      ),
+      body: Column(
+        children: [
+          FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return Expanded(
+                    child: ListView.builder(
+                        itemCount: postlist.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                "Title".text.semiBold.size(16).make(),
+                                Text(postlist[index].title.toString()),
+                                "Description".text.semiBold.size(16).make(),
+                                Text(postlist[index].body.toString())
+                              ],
+                            ),
+                          );
+                        }),
+                  );
+                }
+              })
+        ],
+      ),
+    );
   }
 }
