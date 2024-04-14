1ed18cfe381c2b0bde14cee3c962fee53bef1319
princechaudhary874 <princechaudhary0001001@gmail.com>
2024-04-14 16:42:18 +0545

    shared preference using mvvm

--- mvvm_architecture/lib/main.dart
+++ mvvm_architecture/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:mvvm_architecture/utils/routes/routes.dart';
 import 'package:mvvm_architecture/utils/routes/routes_name.dart';
 import 'package:mvvm_architecture/view/login_screen.dart';
 import 'package:mvvm_architecture/view_model/auth_view_model.dart';
+import 'package:mvvm_architecture/view_model/user_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -17,9 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => AuthViewModel(),
-        )
+        ChangeNotifierProvider(create: (_) => AuthViewModel()),
+        ChangeNotifierProvider(create: (_) => UserViewModel())
       ],
       child: MaterialApp(
         title: 'MVVM architecture',
---
+++ mvvm_architecture/lib/view_model/user_view_model.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserViewModel with ChangeNotifier {}
